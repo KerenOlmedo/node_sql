8e28180e6179e36769fb90e9be2db08495959bd0
luisclaudiorp <61363429+luisclaudiorp@users.noreply.github.com>
2022-03-13 20:14:45 -0300

    add post, update, delete

--- index.js
+++ index.js
@@ -13,4 +13,56 @@ app.get('/user', async (req, res) => {
   res.json(data).status(200);
 });
 
+app.get('/user/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const query = `SELECT * FROM usuario WHERE idusuario = ${id};`;
+    const data = await executaQuery(query);
+    if (data.length <= 0) {
+      throw new Error('Usuario não encontrado!');
+    }
+    res.json(data).status(200);
+  } catch (error) {
+    res.send({ error: error.message }).status(500);
+  }
+});
+
+app.post('/user', async (req, res) => {
+  try {
+    const {
+      nome, email, senha, status, telefone,
+    } = req.body;
+    const query = `INSERT INTO usuario(nome, email, senha, status, telefone) VALUE ("${nome}", "${email}", "${senha}", "${status}", "${telefone}");`;
+    const data = await executaQuery(query);
+    res.json(data).status(201);
+  } catch (error) {
+    res.send(error).status(500);
+  }
+});
+
+app.put('/user/:id', async (req, res) => {
+  try {
+    const {
+      nome, email, senha, status, telefone,
+    } = req.body;
+    const { id } = req.params;
+    const query = `UPDATE usuario SET nome = "${nome}", email = "${email}", senha = "${senha}", status = "${status}", telefone = "${telefone}" WHERE idusuario = ${id};`;
+    await executaQuery(query);
+    res.json({ message: 'Usuario modificado!' }).status(201);
+  } catch (error) {
+    res.send(error).status(500);
+  }
+});
+
+app.delete('/user/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const query = `DELETE FROM usuario WHERE idusuario = ${id};`;
+    await executaQuery(query);
+    res.json({ message: 'Usuario Apagado!' }).status(201);
+  } catch (error) {
+    res.send(error).status(500);
+  }
+});
+
 app.listen(PORT, () => console.log(`Server listen on port: ${PORT}`));
